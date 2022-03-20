b440c8d9a8db66a286b23c6da7793a2f4e4d4b28
Wellington Oliveira <jwell.oliveira@gmail.com>
2022-03-20 11:18:27 -0300

    alteração index

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.send("index");
+  res.render("index");
 });
 
 app.get("/index", (req, res) => {
