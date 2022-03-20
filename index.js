5567b9ac319998e147a320b8b0b394dd55da0f45
Wellington Oliveira <jwell.oliveira@gmail.com>
2022-03-20 11:25:56 -0300

    alteração index rotas

--- index.js
+++ index.js
@@ -12,7 +12,15 @@ app.get("/", (req, res) => {
 });
 
 app.get("/index", (req, res) => {
-    res.render("index");
-  });
+  res.render("index");
+});
+
+app.get("/cadastro", (req, res) => {
+  res.render("cadastro");
+});
+
+app.get("/detalhes", (req, res) => {
+  res.render("detalhes");
+});
 
 app.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}`));
